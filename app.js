8ca750ba56849f0070cb59f21e539a43e4b134e8
Kubi Kubisiak <mckubisiak@gmail.com>
2021-03-25 09:19:26 -0700

    set fondation code for bet project

---
+++ app.js
@@ -0,0 +1,16 @@
+let currentWallet = 1000;
+const currentBet = document.getElementById ('current-bet')
+const makeBet =  document.getElementById ('make-bet')
+
+
+makeBet.addEventListener('click', () => {
+    const number = Number(currentBet.value)
+    const results = Math.random();
+        if (results > 0.5) {
+            
+            
+        }
+    console.log(results)
+
+})
+
